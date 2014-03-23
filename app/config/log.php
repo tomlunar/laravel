e455b7de7f709c83fefff620286c87dfe07eec20
Tom Lunar <tomlunar@outlook.com>
2014-03-23 01:36:55 +0100

    Make the default Laravel log file configurable.

---
+++ app/config/log.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Log file
+	|--------------------------------------------------------------------------
+	|
+	| By default we will build a basic log file setup which creates a single
+	| file for logs. The artisan command tail uses this file both locally
+	| and remotely, though it can be overriden using the --path option.
+	|
+	*/
+
+	'file' => storage_path().'/logs/laravel.log',
+
+);
