7a482826824e93e1f6335d6ffc3f290810233350
Tom Lunar <tomlunar@outlook.com>
2014-03-23 02:50:24 +0100

    Make the app directory structure configurable.

--- bootstrap/paths.php
+++ bootstrap/paths.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Application Paths
+|--------------------------------------------------------------------------
+|
+| Laravel allows you to use the directory structure that fits your needs.
+| Note that you might also need to update composer.json and global.php, 
+| and that some paths are not set here as not used by the framework.
+|
+*/
+
 return array(
 
 	/*
@@ -53,5 +64,96 @@ return array(
 	*/
 
 	'storage' => __DIR__.'/../app/storage',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Commands Path
+	|--------------------------------------------------------------------------
+	|
+	| You may store your custom commands in this folder. This default path is 
+	| used by the command:make command, though you can set it using --path.
+	| Change this path here and update your composer.json and global.php.
+	|
+	*/
+
+	'commands' => __DIR__.'/../app/commands',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Configuration Path
+	|--------------------------------------------------------------------------
+	|
+	| The configuration path contains the configuration files of your Laravel
+	| application, for cache, database, mail, session, etc... You can move
+	| or rename this configuration path here, to accomodate your needs.
+	|
+	*/
+
+	'config' => __DIR__.'/../app/config',
+	
+	/*
+	|--------------------------------------------------------------------------
+	| Controllers Path
+	|--------------------------------------------------------------------------
+	|
+	| Controllers may be store in the controllers path. This default path is
+	| used by some artisan commands to generate controllers. If you want to
+	| change it, also update your composer.json and the start/global.php.
+	|
+	*/
+
+	'controllers' => __DIR__.'/../app/controllers',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Language Path
+	|--------------------------------------------------------------------------
+	|
+	| The language strings are stored in files inside the app/lang directory,
+	| which is the default path. It contains subfolders for each language,
+	| where resides the files. You can specify your own path if needed.
+	| 
+	*/
+
+	'lang' => __DIR__.'/../app/lang',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Migrations Path
+	|--------------------------------------------------------------------------
+	|
+	| Migrations are a type of version control for your database. This folder
+	| is used to store your migration classes. If for some reasons you need
+	| to change this path, don't forget to update the composer.json file.
+	| 
+	*/
+
+	'migrations' => __DIR__.'/../app/database/migrations',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Start Path
+	|--------------------------------------------------------------------------
+	|
+	| Files inside the start path are related to your application. In the life
+	| request cycle they are loaded once the framework has been initialized.
+	| If for some reason you want to change that path, you can do it here.
+	|
+	*/
+
+	'start' => __DIR__.'/../app/start',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Views Path
+	|--------------------------------------------------------------------------
+	|
+	| Here you may set the default path that should be checked for your views.
+	| This path is the primary views path used by the framework ; note that
+	| you may specify additional views paths in the config/view.php file.
+	|
+	*/
+
+	'views' => __DIR__.'/../app/views',
 
 );
